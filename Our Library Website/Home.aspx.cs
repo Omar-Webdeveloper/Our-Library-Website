45c706f316f083c82630c5162ef44e5f2e48ee5b
Omar-Webdeveloper <omarabojolban9@gmail.com>
2025-02-04 00:59:34 +0300

    Add project files.

---
+++ Our Library Website/Home.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Our_Library_Website
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(File.ReadAllText(Server.MapPath("~/App_Data/LoggedUser.txt"))))
+            {
+                signIn.Visible = true;
+                logIn.Visible = true;
+            }
+            else
+            {
+                profile.Visible = true;
+                lnkLogout.Visible = true;
+            }
+        }
+        protected void lnkLogout_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(Server.MapPath("~/App_Data/LoggedUser.txt"), string.Empty);
+            profile.Visible = false;
+            lnkLogout.Visible = false;
+            signIn.Visible = true;
+            logIn.Visible = true;
+        }
+
+        protected void borrow_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(File.ReadAllText(Server.MapPath("~/App_Data/LoggedUser.txt"))))
+            {
+                Response.Redirect("Users/loginPage.aspx");
+            }
+            else
+            {
+                Response.Redirect("Books/SearchBooks.aspx");
+            }
+        }
+    }
+}
